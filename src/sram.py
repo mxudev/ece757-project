3e1af5ed9b84629dc22f81efe0e685bc854ae875
Jerry Xu <mxudev@gmail.com>
2024-05-10 21:27:11 -0500

    stuff

--- src/sram.py
+++ src/sram.py
@@ -11,6 +11,7 @@ class SramState:
         self.beta = 1 - alpha
         self.alpha_size = size * alpha
         self.beta_size = size * self.beta
+        self.f_preload_size = 0
 
     def operate_conv(self, in_shpae, out_shape, conv_shape):
         #-------Kernel Accesss-------
@@ -18,11 +19,15 @@ class SramState:
         K = conv_shape[0] #number of kernel vectors
         F = out_shape[1] * out_shape[2] #number of feature vectors
         k_size = conv_shape[1] * conv_shape[2] * conv_shape[3] * PERCISON #kernel vector size in bytes
-        dram_ld_kernel = math.ceil(F/SA_H) * math.ceil(K * k_size / BUSWIDTH)
+        dram_ld_kernel = math.ceil(F/SA_H) * (K * k_size - self.alpha_size) / BUSWIDTH
         #SRAM ld / st
         sram_ld_kernel = math.ceil(F/SA_H) * K
-        sram_st_kernel = dram_ld_kernel
+        sram_st_from_dram_kernel = dram_ld_kernel
 
         #-------Festure Accesss-------
+        #DRAM ld
+        #TODO
+
+
         
 
